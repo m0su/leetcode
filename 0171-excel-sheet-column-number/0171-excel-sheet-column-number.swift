05d216e73a224307b1ae9ca74aa919fae31fa9b3
thingsu <sirm0su@gmail.com>
2022-11-30 13:51:43 +0900

    Time: 8 ms (68.38%), Space: 14 MB (78.63%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.swift
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.swift
@@ -2,7 +2,7 @@ class Solution {
   func titleToNumber(_ columnTitle: String) -> Int {
     // 26n+a
     // asciiValue = unicodeScalars.first!.value
-    return columnTitle.reduce(0) { $0 * 26 + Int(($1.asciiValue! - 64))}    
-    // return columnTitle.unicodeScalars.reduce(0) { $0 * 26 + Int($1.value - 64) }
+    // return columnTitle.reduce(0) { $0 * 26 + Int(($1.asciiValue! - 64))}    
+    return columnTitle.unicodeScalars.reduce(0) { $0 * 26 + Int($1.value - 64) }
   }
 }
